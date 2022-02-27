cf62f436b65f2e27a0fde7bf159fd3711d4d43af
Djembeleza <kaola730@gmail.com>
2022-02-27 11:16:37 +0200

    Changed the footer details

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
   return (
     <footer className="mx-auto mt-48 text-center">
       <a
-        href="https://www.pointer.gg?utm_source=thirdweb-lootbox"
+        href="https://bio.link/djembeleza"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Learn web3 dev and earn crypto rewards at{" "}
-        <span className="underline">Pointer</span>
+        Coded by{" "}
+        <span className="underline">Djembeleza</span>
       </a>
     </footer>
   );
